0e4691ad9ba404a28d658169a416e4ff223c800a
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-12-19 13:23:31 +0545

    feat : microservices project  structure

--- apps/payement/src/main.ts
+++ apps/payement/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { PayementModule } from './payement.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(PayementModule);
-  await app.listen(process.env.port ?? 3000);
+  const RABBITMQ_URL = 'amqp://guest:guest@localhost:5672';
+
+  app.enableCors();
+  await app.listen(process.env.port ?? 3001);
+
+  const microservice = app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [RABBITMQ_URL],
+      queue: 'payement_queue',
+      queueOptions: { durable: true },
+    },
+  });
+
+  await microservice.listen();
+
+  Logger.log('Application  is listening on rabbitmq');
 }
 bootstrap();
